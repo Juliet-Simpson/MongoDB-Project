4c0efecf6a672919aaf97410b5e97b9b6aedcfd3
Juliet-Simpson <simpsonjulietc@gmail.com>
2021-06-23 11:33:44 +0000

    Add rest of commented out code fore reference

--- mongo.py
+++ mongo.py
@@ -35,24 +35,24 @@ coll = conn[DATABASE][COLLECTION]
 # coll.insert(new_doc)
 
 """ Insert multipe documents """
-new_docs = [{
-    "first": "terry",
-    "last": "pratchett",
-    "dob": "28/04/1948",
-    "gender": "m",
-    "hair_color": "not much",
-    "occupation": "writer",
-    "nationality": "british"
-}, {
-    "first": "george",
-    "last": "rr martin",
-    "dob": "20/09/1948",
-    "gender": "m",
-    "hair_color": "white",
-    "occupation": "writer",
-    "nationality": "american"
-}]
-coll.insert_many(new_docs)
+# new_docs = [{
+#     "first": "terry",
+#     "last": "pratchett",
+#     "dob": "28/04/1948",
+#     "gender": "m",
+#     "hair_color": "not much",
+#     "occupation": "writer",
+#     "nationality": "british"
+# }, {
+#     "first": "george",
+#     "last": "rr martin",
+#     "dob": "20/09/1948",
+#     "gender": "m",
+#     "hair_color": "white",
+#     "occupation": "writer",
+#     "nationality": "american"
+# }]
+# coll.insert_many(new_docs)
 
 documents = coll.find()
 
